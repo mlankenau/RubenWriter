f6a73a272eec693b28bc7bb697c99ea09ba762d1
marcus <marcus.lankenau@gmail.com>
2011-03-16 20:33:10 +0100

    formating

--- src/main/java/de/lankenau/rubenwrite/MainFrame.java
+++ src/main/java/de/lankenau/rubenwrite/MainFrame.java
@@ -217,7 +217,6 @@ public class MainFrame extends JFrame {
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
