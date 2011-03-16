98d09489ba5f65f510af989c7086bd031572e598
marcus <marcus.lankenau@gmail.com>
2011-03-16 20:41:21 +0100

    read stream when exec iptables

--- src/main/java/de/lankenau/rubenwrite/Network.java
+++ src/main/java/de/lankenau/rubenwrite/Network.java
@@ -1,22 +1,34 @@
 package de.lankenau.rubenwrite;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class Network {
 	public static void start() {		
 		Process p;
 		try {
 			p = Runtime.getRuntime().exec("/sbin/iptables", new String[] { "--delete", "OUTPUT",  "1"});
+			readStream(p);
 			p.waitFor();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}	    		
 	}
 
+	private static void readStream(Process p) throws IOException {
+		InputStreamReader reader = new InputStreamReader(p.getInputStream());
+		int c;
+		while ((c = reader.read()) != -1) {
+			System.out.print((char)c);
+		}
+	}
+
 	public static void stop() {		
 		Process p;
 		try {
 			p = Runtime.getRuntime().exec("/sbin/iptables", new String[] { "-A", "OUTPUT", "-j", "DROP"});
+			readStream(p);
 			p.waitFor();
 		} catch (Exception e) {
 			e.printStackTrace();
